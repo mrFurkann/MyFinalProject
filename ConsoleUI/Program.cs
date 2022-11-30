7298fdb1022a561aa99357b8766327a9be017988
Furkan <furkankosar2005@gmail.com>
2022-11-30 15:38:50 +0300

    Update

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,18 +11,20 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //ProductTest();
+            ProductTest();
 
-            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
-            foreach (var customer in customerManager.GetAll())
-            {
-                Console.WriteLine(customer.ContactName);
-            }
+            //CategoryTest();
 
         }
 
-
-
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
 
 
 
@@ -35,9 +37,9 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(5, 30))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             };
         }
     }
--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -1,6 +1,5 @@
 ﻿using Core.DataAccess;
 using Entities.Concrete;
-using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +8,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal:IEntityRepository<Product>
+    public interface ICategoryDal:IEntityRepository<Category>
     {
-        List<ProductDetailDto> GetProductDetails() ;
-       
     }
 }
-
-//Code Refactoring
